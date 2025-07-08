9f0480b68a0c2d8b3bb7b02a76fb71d574f0eb5e
Chandranil Adhikary <chandraniladhikary16dec2003@gmail.com>
2025-07-08 13:27:08 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -5,7 +5,10 @@ require('dotenv').config();
 const authrouter = require('./routes/Auth');
 const Noterouter = require('./routes/Noteroute');
 const mongoconnect= require('./db/Database');
-app.use(cors());
+app.use(cors({
+  origin: "https://mynotes-app-frontend.onrender.com", 
+  credentials: true
+}));
 app.use(express.json());
 app.use('/api/auth',authrouter);
 app.use('/api/notes', Noterouter);
