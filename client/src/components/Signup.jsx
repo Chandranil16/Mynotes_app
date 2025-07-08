35d9071983365a778fc34b049767940f90f0d9c4
Chandranil Adhikary <chandraniladhikary16dec2003@gmail.com>
2025-07-08 12:55:23 +0530

    Update Signup.jsx

--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -23,7 +23,7 @@ const Signup = () => {
     }
 
     try {
-      const res = await axios.post("http://localhost:5000/api/auth/register", {
+      const res = await axios.post("https://mynotes-app-backend.onrender.com/api/auth/register", {
         name,
         email,
         password,
