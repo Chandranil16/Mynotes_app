294cbd276b28dec6428df50914a0d6c8d0ffcd3e
Chandranil Adhikary <chandraniladhikary16dec2003@gmail.com>
2025-07-08 12:53:41 +0530

    Update Home.jsx

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -31,7 +31,7 @@ const Home = () => {
 
   const fetchnotes = async () => {
     try {
-      const { data } = await axios.get("http://localhost:5000/api/notes", {
+      const { data } = await axios.get("https://mynotes-app-backend.onrender.com/api/notes", {
         headers: {
           Authorization: `Bearer ${localStorage.getItem("token")}`,
         },
@@ -54,7 +54,7 @@ const Home = () => {
   const addnote = async (title, desc) => {
     try {
       const res = await axios.post(
-        "http://localhost:5000/api/notes/add",
+        "https://mynotes-app-backend.onrender.com/api/notes/add",
         { title, desc },
         {
           headers: {
@@ -84,7 +84,7 @@ const Home = () => {
 
   const deletenote = async (id) => {
     try {
-      const res = await axios.delete(`http://localhost:5000/api/notes/${id}`, {
+      const res = await axios.delete(`https://mynotes-app-backend.onrender.com/api/notes/${id}`, {
         headers: {
           Authorization: `Bearer ${localStorage.getItem("token")}`,
         },
@@ -102,7 +102,7 @@ const Home = () => {
   const editnote = async (id, title, desc) => {
     try {
       const res = await axios.put(
-        `http://localhost:5000/api/notes/${id}`,
+        `https://mynotes-app-backend.onrender.com/api/notes/${id}`,
         { title, desc },
         {
           headers: {
